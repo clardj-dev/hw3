83f6a01b273498affc1cdb84741e8c9534732e18
David Clark <david.clark1@kellogg.northwestern.edu>
2025-05-20 19:53:54 +0000

    Added actions to controllers and new Places view

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,46 @@
 class EntriesController < ApplicationController
+
+  def index
+    # redirect user
+    redirect_to "/entries"
+  end
+  
+  def show
+    # redirect user
+    redirect_to "/entries"
+  end
+
+  def new
+    # render view with new Entry form
+  end
+
+  def create
+    # start with a new Entry
+    @entry = Entry.new
+
+    # assign user-entered form data to Entry's columns
+    @entry["name"] = params["name"]
+
+    # save Entry row
+    @entry.save
+
+    # redirect user
+    redirect_to "/entries"
+  end
+
+  def edit
+    # redirect user
+    redirect_to "/entries"
+  end
+
+  def update
+    # redirect user
+    redirect_to "/entries"
+  end
+
+  def destroy
+    # redirect user
+    redirect_to "/entries"
+  end
+
 end
